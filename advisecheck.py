f790c368af43bf3c8723dc32fe5bc09bcc8ee11b
NilogTheUseless <88753140+NilogTheUseless@users.noreply.github.com>
2022-03-29 18:12:02 -0500

    Update advisecheck.py

--- advisecheck.py
+++ advisecheck.py
@@ -2,6 +2,13 @@ import requests as re
 
 response = re.get('https://www.localconditions.com/weather-wichita-kansas/67201/alerts.php')
 html = response.text
+
+import cgi
+
+form = cgi.FieldStorage()
+
+alerts = form.getValue("alerts")
+
 index = open("index.html").read().format(bla-bla='something',
                                         wowo='wawa')
 
