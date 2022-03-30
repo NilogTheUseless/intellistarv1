ebb672941f85578e08e01ca49f1b783efea95ef3
NilogTheUseless <88753140+NilogTheUseless@users.noreply.github.com>
2022-03-29 19:40:51 -0500

    Update advisecheck.py

--- advisecheck.py
+++ advisecheck.py
@@ -2,12 +2,15 @@ import requests as re
 
 response = re.get('https://www.localconditions.com/weather-wichita-kansas/67201/alerts.php')
 html = response.text
+file = open("alerts.txt", "w")
+file.write(html)
 
-import cgi
 
-form = cgi.FieldStorage()
-
-alerts = form.getValue("alerts")
+word = html
+if word.find('<h5 class="card-title">Wind Advisory issued') != -1):
+  windadvise = false
+else:
+  windadvise = 
 
 index = open("index.html").read().format(bla-bla='something',
                                         wowo='wawa')
